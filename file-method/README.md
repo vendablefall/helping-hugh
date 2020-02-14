b8bb6e7df578590597635bbaf892333808f3f059
Alex Simpson <alexjsimpson@gmail.com>
2020-02-15 09:37:31 +1300

    adding show

--- file-method/README.md
+++ file-method/README.md
@@ -1,1 +1,4 @@
 #### Dennis' Idea
+
+This basically takes a users input and stores it in a txt file, it also reads the text file and displays the contents.
+
--- file-method/write.php
+++ file-method/write.php
@@ -6,11 +6,4 @@
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
-
-// https://stackoverflow.com/questions/17871483/read-form-text-file-and-write-in-html
- $dbinfo = fopen("comments.txt", "r") or die("Unable to open db!");
- echo $dbinfo,
-//  echo '<textarea>', htmlspecialchars($data), '</textarea>';
- fclose($dbinfo);
-
 ?>
