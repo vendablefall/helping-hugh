e73a3bc95e6bea31d1e94e3d4b1747ce7f826186
Alex Simpson <alexjsimpson@gmail.com>
2020-02-15 09:32:51 +1300

    fin?

--- file-method/action.php
+++ file-method/action.php
@@ -1,9 +1,16 @@
 <?php
  $path = 'comments.txt';
- if (isset($_POST['field1']) && isset($_POST['field2'])) {
+ if (isset($_POST['name']) && isset($_POST['comment'])) {
     $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
+    $string = $_POST['name'].' - '.$_POST['comment'];
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
+
+// https://stackoverflow.com/questions/17871483/read-form-text-file-and-write-in-html
+ $dbinfo = fopen("comments.txt", "r") or die("Unable to open db!");
+ echo $dbinfo,
+//  echo '<textarea>', htmlspecialchars($data), '</textarea>';
+ fclose($dbinfo);
+
 ?>
