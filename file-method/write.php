ff4e10cc8e1e14f6cc1df41e76a7f505997ced6a
Alex Simpson <alexjsimpson@gmail.com>
2020-02-15 09:58:55 +1300

    comments

--- file-method/write.php
+++ file-method/write.php
@@ -2,7 +2,7 @@
  $path = 'comments.txt';
  if (isset($_POST['name']) && isset($_POST['comment'])) {
     $fh = fopen($path,"a+");
-    $string = $_POST['name'].' - '.$_POST['comment'];
+    $string = $_POST['name'].' - '.$_POST['comment']; // Set the writing format here
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
